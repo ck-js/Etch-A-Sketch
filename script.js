76829ad3187a012c31f46c6be64fbeb6092db748
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-07-09 21:19:14 +0700

    Add 16 square divs using a for loop to create the div element and set its style and attributes using template literals with the loops iterator to also set the text content. Append the square divs to our container div.

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const gridContainer = document.querySelector('.grid-container')
-const gridSquare = document.createElement('div');
-
+gridContainer.style.cssText = 'display: flex;; flex-direction: column;'
 gridContainer.style.cssText = 'height: 100vh; border: 20px solid yellow;'
 
-gridSquare.style.cssText = 'background-color: pink; height: 200px; width: 200px;'
-gridSquare.textContent = '1';
 
-gridContainer.appendChild(gridSquare)
+let numberOfSquares = 16;
+for (let i = 0; i < numberOfSquares; i++) {
+    const square = document.createElement('div');
+
+    square.textContent = `SQUARE ${i}`;
+    square.style.cssText = 'background-color: pink; height: 200px; width: 200px; border: 5px solid black;'
+square.setAttribute('id', `${i}`)    
+
+    gridContainer.appendChild(square)
+}
+
+
+
+
+
+
 
