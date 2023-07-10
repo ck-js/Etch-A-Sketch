9125b6b424dad6f08a7f33e644cd4b25ef453c1c
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-07-10 14:30:48 +0700

    Add grid layout using grid template property.

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const gridContainer = document.querySelector('.grid-container')
+const Container = document.querySelector('.grid-container')
 
-let numberOfSquares = 32;
+let numberOfSquares = 256;
 for (let i = 1; i <= numberOfSquares; i++) {
     const square = document.createElement('div');
 
     square.textContent = `SQUARE ${i}`;
-    square.style.cssText = 'background-color: pink; height: 200px; width: 200px; border: 5px solid black;'
+    square.style.cssText = 'background-color: purple; border: 2px solid black;'
 square.setAttribute('id', `${i}`)    
 
-    gridContainer.appendChild(square)
+    Container.appendChild(square)
 }
 
 
-gridContainer.style.cssText = 'display: flex;;  flex-wrap: wrap;'
-gridContainer.style.border = '10px solid yellow'
+// Container.style.cssText = 'display: flex;;  flex-wrap: wrap;'
+Container.style.border = '10px solid yellow'
 
 
 
