04a978175311401f0bd8d64c30a481990acc61a4
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-07-10 17:19:59 +0700

    Add element that contains nosde list array of square divs, iterate through each square div using for each method. Add event listener of mouse over to each element and anonymous function to change the background color of each square div when mouse over event is triggered.

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ for (let i = 1; i <= numberOfSquares; i++) {
     square.textContent = `SQUARE ${i}`;
     square.style.cssText = 'background-color: purple; border: 2px solid black;'
 square.setAttribute('id', `${i}`)    
+square.setAttribute('class', 'square')
 
     Container.appendChild(square)
 }
 
-
-// Container.style.cssText = 'display: flex;;  flex-wrap: wrap;'
 Container.style.border = '10px solid yellow'
 
-
+const squares = document.querySelectorAll('.square')
+squares.forEach((square) => {
+square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'white';
+})    
+})
 
 
